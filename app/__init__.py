8151ca1efd1296df7b5794fc01a7900a33d68b23
xiaoxiaoki <1241908493@qq.com>
2018-06-28 18:42:16 +0800

    rest

--- app/__init__.py
+++ app/__init__.py
@@ -53,6 +53,9 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
+    from .api import api as api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix='/api/v1.0')
+
     # 附加路由和自定义的错误页面
 
     return app  # 工厂函数返回创建的程序示例
--- app/main/errors.py
+++ app/main/errors.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 # 蓝本中的错误处理程序
-from flask import render_template
+from flask import render_template, request, jsonify
 from . import main
 """
 在蓝本中编写错误处理程序稍有不同,如果使用 errorhandler 修饰器,那么只有蓝本中的
@@ -8,11 +8,30 @@ from . import main
 """
 
 
+@main.app_errorhandler(403)
+def forbidden(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'forbidden'})
+        response.status_code = 403
+        return response
+    return render_template('403.html'), 403
+
+
 @main.app_errorhandler(404)
 def page_not_found(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        # 检查Accept请求首部,Werkzeug将其解码为request.accept_imetypes,根据首部的值决定客户端期望接收的响应格式
+        # 浏览器一般不限制响应的格式，所以只为接受JSON格式而不接受HTML格式的客户端生成JSON格式响应
+        response = jsonify({'error': 'not found'})
+        # jsonify将我们传入的json形式数据序列化成为json字符串,作为响应的body,并且设置响应的Content-Type为application/json,构造出响应返回至客户端
+        response.status_code = 404
+        return response
     return render_template('404.html'), 404
 
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
